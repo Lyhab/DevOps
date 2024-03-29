b93688e442676934e26f61e2c3c153f15e890aa6
Lyhab <Lyhabrithyny@gmail.com>
2024-03-29 16:21:04 +0700

    1st commit

---
+++ src/test/java/i4/s2/devops/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package i4.s2.devops.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
