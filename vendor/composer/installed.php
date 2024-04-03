f460d7bb386704143d183692a9dd026cad513c1e
Lyhab <Lyhabrithyny@gmail.com>
2024-04-03 21:30:02 +0700

    TP03

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '1473e63ce1625b321474cee19ae9bb5d2f9b4a18',
+        'reference' => '2200992c6251b304a41508731dc26dbaf04b9d96',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '1473e63ce1625b321474cee19ae9bb5d2f9b4a18',
+            'reference' => '2200992c6251b304a41508731dc26dbaf04b9d96',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -757,8 +757,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
